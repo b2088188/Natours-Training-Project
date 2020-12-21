02c97ea78892e69f3c1782e220553fc215990077
Shunze Lin <4A490101@stust.edu.tw>
2020-12-21 22:51:58 +0800

    Simple refactoring for MVC

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import AppError from'./utils/appError.js'
 import tourRouter from'./routes/tourRoutes.js'
 import userRouter from'./routes/userRoutes.js'
 import reviewRouter from'./routes/reviewRoutes.js'
+import viewRouter from './routes/viewRoutes.js';
 import globalErrorHandler from './controllers/errorController.js'
 
 const app = express();
@@ -25,14 +26,7 @@ app.use(express.static(join(__dirname, 'public')));
 
 
 //Mounting
-app.get('/', (req, res, next) => {
-  //Look for base pug to render
-  res.status(200).render('base', {
-  	//Passing variables: locals
-  	tour: 'The Forest Hiker',
-  	user: 'Shunze'
-  });
-});
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
---
+++ controllers/viewController.js
@@ -0,0 +1,13 @@
+
+
+export const getOverview =  (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  })
+}
+
+export const getTour =  (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour'
+  })
+}
