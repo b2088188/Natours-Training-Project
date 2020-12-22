fac810d62f82649bad53f2eada8e12e17beea854
Shunze Lin <4A490101@stust.edu.tw>
2020-12-22 22:33:31 +0800

    feat: Implementing Error Page

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,33 +5,43 @@ const globalErrorHandler = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
 	err.status = err.status || 'error';
     if(process.env.NODE_ENV === 'development')    	
-	  return sendErrorDev(err, res);
+	  return sendErrorDev(err, req, res);
 	let error = {...err};
+	error.message = err.message;
       if(error.name === 'CastError') 
      error = handleCastErrorDB(error)      
      if(error.name === 'JsonWebTokenError')
      	error = handleJWTError();
      if(error.name === 'TokenExpiredError')
      	error = handleJWTExpiredError();
-	    sendErrorProd(error, res);    	  
+	    sendErrorProd(error, req, res);    	  
 }
 export default globalErrorHandler;
 
 //Error in Dev mode
-function sendErrorDev(err, res) {
-	res.status(err.statusCode).json({
-			status: err.status,
-			error: err,
-			message: err.message,
-			stack: err.stack
-		});
+function sendErrorDev(err, req, res) {
+	//API
+	if(req.originalUrl.startsWith('/api'))      
+	return res.status(err.statusCode).json({
+					status: err.status,
+					error: err,
+					message: err.message,
+					stack: err.stack
+				});
+    //Rendered Website
+    res.status(err.statusCode).render('error', {
+    	title: 'Something went wrong ⚠',
+    	msg: err.message
+    })
 }
 
 //Error in Prod mode
-function sendErrorProd(err, res) {
-	//Operational, trusted error: send message to Client
+function sendErrorProd(err, req, res) {
+	//API
+	if(req.originalUrl.startsWith('/api')){
+		//Operational, trusted error: send message to Client
 	if(err.isOperational)		
-	res.status(err.statusCode).json({
+	return res.status(err.statusCode).json({
 			status: err.status,
 			message: err.message
 		});
@@ -39,10 +49,26 @@ function sendErrorProd(err, res) {
 	// 1) Log Error
 	console.error('ERROR ☹', err);
 	// 2) Send generic message
-	res.status(500).json({
+	return res.status(500).json({
 		status: 'error',
 		message: 'Something went very wrong!'
 	})
+	}   
+
+	//Rendered Website
+	if(err.isOperational)		
+	return res.status(err.statusCode).render('error', {
+    	title: 'Something went wrong ⚠',
+    	msg: err.message
+    })
+	//Programming or other unknown error: don't provide error details
+	// 1) Log Error
+	console.error('ERROR ☹', err);
+	// 2) Send generic message
+	return res.status(err.statusCode).render('error', {
+    	title: 'Something went wrong ⚠',
+    	msg: 'Please try again later'
+    })
 }
 
 function handleCastErrorDB(err) {
--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,12 +1,12 @@
 import Tour from '../models/tourModel.js';
 import catchAsync from '../utils/catchAsync.js';
+import AppError from '../utils/appError.js';
 
 
 export const getOverview =  catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
   // 2) Build template
-
   // 3) Render that template using tour data from (1)
   res.status(200).render('overview', {
     title: 'All Tours',
@@ -19,6 +19,8 @@ export const getTour = catchAsync(async (req, res, next) => {
     path: 'reviews',
     fields: 'review rating user'
   })
+    if(!tour)
+    return next(new AppError('There is no tour with that name', 404));
   res.status(200).render('tour', {
     title: `${tour.name} tour`,
     tour
