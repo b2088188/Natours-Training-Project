da493f08421743aba3777b336183698cb3ade580
Shunze Lin <4A490101@stust.edu.tw>
2020-12-22 13:26:18 +0800

    feat: SSR for overview

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,13 +1,25 @@
+import Tour from '../models/tourModel.js';
+import catchAsync from '../utils/catchAsync.js';
 
+export const getOverview =  catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+  // 2) Build template
 
-export const getOverview =  (req, res) => {
+  // 3) Render that template using tour data from (1)
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   })
-}
+})
 
-export const getTour =  (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+export const getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({slug: req.params.slug}).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   })
-}
+  res.status(200).render('tour', {
+    title: `${tour.name} tour`,
+    tour
+  })
+})
